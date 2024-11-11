0f71e530ed29df9a713df80e7e90566d5e71ef10
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-11-11 16:52:26 -0500

    Create the index and main component

--- app/_layout.js
+++ app/_layout.js
@@ -1,9 +1,10 @@
+import { Slot } from "expo-router";
 import { View, Text } from "react-native";
 
 export default function Layout() {
   return (
     <View>
-      <Text>Hola</Text>
+      <Slot />
     </View>  
   );
 }
---
+++ components/Main.jsx
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+
+export function Main() {
+  return (
+    <View>
+      <Text>Main component
+      </Text>
+    </View>
+  )
+}
