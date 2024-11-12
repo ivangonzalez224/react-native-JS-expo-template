98058620a5545ae529f94e82d7e39cadb26a6b6a
Iván Gonzalez <ivan.gonzalezr224@gmail.com>
2024-11-12 12:18:45 -0500

    Install expo icons

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,12 +1,13 @@
 import { Link } from "expo-router"; 
 import { Pressable, ScrollView, Text, ActivityIndicator } from "react-native";
+import FontAwesome from '@expo/vector-icons/Fontawesome';
 
 export function Main() {
   return (
     <ScrollView>
       <Link asChild href="/">
         <Pressable>
-          <Text>Main component</Text>
+          <FontAwesome name="home" />
         </Pressable>
     </ScrollView>
   )
